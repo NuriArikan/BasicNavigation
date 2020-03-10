76245b64de192c813474e4fef7995f8e6bc3ad9f
NuriArikan <nuri@cornelldecor.com>
2020-03-10 15:22:20 -0400

    my first commit

--- src/test/java/com/automation/tests/FirstClass.java
+++ src/test/java/com/automation/tests/FirstClass.java
@@ -4,12 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("Hello, Selenium!");
-
-
-
-
-
-
-
+        System.out.println("Terminal, Git and Github");
     }
 }
