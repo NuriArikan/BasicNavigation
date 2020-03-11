e2e5f4f086a934d57690d35da24a524789ee33de
NuriArikan <nuri@cornelldecor.com>
2020-03-11 17:42:32 -0400

    initial commit

--- src/test/java/com/automation/tests/FirstClass.java
+++
@@ -1,9 +0,0 @@
-package com.automation.tests;
-
-public class FirstClass {
-    public static void main(String[] args) {
-
-        System.out.println("Hello, Selenium!");
-        System.out.println("Terminal, Git and Github");
-    }
-}
